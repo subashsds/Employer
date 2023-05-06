3ec0edfb75638740c2a84bf04829c2976c790d81
subashsds <subashsds@gmail.com>
2023-05-06 13:04:21 +0530

    d

--- src/main/java/com/slavic/entity/Employee.java
+++ src/main/java/com/slavic/entity/Employee.java
@@ -3,12 +3,6 @@ package com.slavic.entity;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
 //@Entity
 //@Table(name = "users_tbl")
 public class Employee  implements Serializable{
@@ -17,72 +11,7 @@ public class Employee  implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 
-		@Id
-	    @GeneratedValue
-	    private int id;
-	  
-	    @Column(name = "user_name")
-	    private String username;
-	    
-	    @Column(name = "account")
-	    private String account;
-	    
-	    @Column(name = "status")
-	    private String status;
-	    
-	    @Column(name = "created_at")
-	    private Date  created_at;
-	    
-	    @Column(name = "updated_at")
-	    private Date  updated_at;
 
-		public int getId() {
-			return id;
-		}
-
-		public void setId(int id) {
-			this.id = id;
-		}
-
-		public String getUsername() {
-			return username;
-		}
-
-		public void setUsername(String username) {
-			this.username = username;
-		}
-
-		public String getAccount() {
-			return account;
-		}
-
-		public void setAccount(String account) {
-			this.account = account;
-		}
-
-		public String getStatus() {
-			return status;
-		}
-
-		public void setStatus(String status) {
-			this.status = status;
-		}
-
-		public Date getCreated_at() {
-			return created_at;
-		}
-
-		public void setCreated_at(Date created_at) {
-			this.created_at = created_at;
-		}
-
-		public Date getUpdated_at() {
-			return updated_at;
-		}
-
-		public void setUpdated_at(Date updated_at) {
-			this.updated_at = updated_at;
-		}
 	    
 	    
 	    
